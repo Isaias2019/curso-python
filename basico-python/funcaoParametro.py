7a6ad55ea5d7562b18c6652e3194e60a88c20453
Isaias2019 <54869705+Isaias2019@users.noreply.github.com>
2025-02-20 05:26:09 +0000

    salvando

---
+++ basico-python/funcaoParametro.py
@@ -0,0 +1,9 @@
+def saudacao(nome):
+    print(f"ola seu nome é {nome}")
+
+seuNome = input('ola me diga seu nome ')
+saudacao(seuNome)
+
+print('ola amigo')
+print('ta tudo bem')
+print('aqui pediu pra instalar o python')
---
+++ basico-python/procurarLista.py
@@ -0,0 +1,11 @@
+# 8. Busca em Lista
+nomes = ["Ana", "Bruno", "Carlos", "Diana"]
+nome_busca = input("Digite o nome que deseja procurar: ")
+if nome_busca in nomes:
+    print(f"{nome_busca} está na lista.")
+else:
+    print(f"{nome_busca} não está na lista.")
+
+    # => conta github @Isaias2019 
+    # essa vai ser a conta principal
+    # Jisaiaslima35 sera a conta do colega
